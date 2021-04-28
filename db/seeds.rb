bc7aa14f9b1490b5f6fe903b6a8783310100826e
xinglin2016 <lixinglin0829@gmail.com>
2021-04-28 11:07:46 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,49 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Creating cocktails..."
 
-Cocktail.create(name: "Mint Julep")
-Cocktail.create(name: "Whiskey Sour")
-Cocktail.create(name: "Mojito")
-
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
+
+require 'json'
+require 'open-uri'
+
+puts "Starting seed"
+
+puts "Creating cocktails..."
+
+def cocktail_name
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  cocktail_serialized = open(url).read
+  return JSON.parse(cocktail_serialized)["drinks"][0]["strDrink"]
+end
+
+def cocktail_description
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  cocktail_serialized = open(url).read
+  return JSON.parse(cocktail_serialized)["drinks"][0]["strInstructions"]
+end
+
+def cocktail_picture
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  cocktail_serialized = open(url).read
+  return JSON.parse(cocktail_serialized)["drinks"][0]["strDrinkThumb"]
+end
+
+def cocktail_ingredients
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  cocktail_serialized = open(url).read
+  return JSON.parse(cocktail_serialized)["drinks"][0]["strIngredient1"]
+end
+
+3.times do
+  cocktail = Cocktail.new(
+    name: cocktail_name
+    )
+  cocktail.save!
+  # file = URI.open(dog[0]["url"])
+  # breed.photo.attach(io: file, filename: 'breed_pic.jpg', content_type: 'image/jpg')
+  # breed.save!
+  puts "Created cocktail #{cocktail.name}"
+end
