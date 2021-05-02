75fdab8f9f23feda1b48803058f6308b64f663e4
xinglin2016 <lixinglin0829@gmail.com>
2021-05-02 17:06:31 -0400

    add description in cocktail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_022457) do
+ActiveRecord::Schema.define(version: 2021_05_02_210335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_022457) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
