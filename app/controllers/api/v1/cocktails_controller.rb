29b19c1e9a2b63580e5f9c52cfc04168c0fd3766
xinglin2016 <lixinglin0829@gmail.com>
2021-05-25 14:30:33 -0400

    add update for api

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CocktailsController < Api::V1::BaseController
-  before_action :set_cocktail, only: [ :show ]
+  before_action :set_cocktail, only: [ :show, :update ]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
 
   def index
     @cocktails = policy_scope(Cocktail)
@@ -8,10 +9,27 @@ class Api::V1::CocktailsController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @cocktail.update(cocktail_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
     authorize @cocktail  # For Pundit
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
